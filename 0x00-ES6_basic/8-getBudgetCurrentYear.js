274968d9b9def907c60c3bd0f369673b4c58acab
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-04-18 20:39:14 +0100

    8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,6 +4,9 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {[`income-${getCurrentYear()}`]: income, [`gdp-${getCurrentYear()}`]: gdp, [`capita-${getCurrentYear()}`]: capita
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 }
