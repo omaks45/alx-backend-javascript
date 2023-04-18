d5f19b4bb1c24eb9f797c5db639edbb9f2466da7
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-04-18 21:14:24 +0100

    9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
-/*eslint-disable import/extension */
+/* eslint-disable import/extensions */
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
