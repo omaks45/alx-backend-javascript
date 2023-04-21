dc8a1eff00168b383224596ee13fdc16903553cd
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-04-21 01:30:09 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default function getResponseAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error('The fake API is not working'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
