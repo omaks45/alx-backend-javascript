9a0e0cb446365ae8939cfde250a1c71dc23895b1
omaks45 <danielgozie5@gmail.com>
2023-04-27 02:08:05 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const Student = [
+    { id: 1, firstName: 'Gaullaume', Location: 'San Francisco' },
+    { id: 2, firstName: 'James', Location: 'Columbia' },
+    { id: 5, firstName: 'Serena', Location: 'San Francisco' },
+  ];
+  return Student;
+}
