5499445cda02f21ba6131bdb93f37285f6eff757
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-04-28 14:24:10 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error('position outside the range');
+    throw Error('Position outside range');
   }
-
   const buffer = new ArrayBuffer(length);
-  const Int8 = new Int8Array(buffer, 0, length);
-  Int8.set([value], position);
+  const int8 = new Int8Array(buffer, 0, length);
+  int8.set([value], position);
   return new DataView(buffer);
 }
