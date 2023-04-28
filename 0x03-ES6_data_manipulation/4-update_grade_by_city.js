4ce9e806f6f08f05d0092c1dd82f5a17dca227bf
omaks45 <danielgozie5@gmail.com>
2023-04-28 01:23:34 +0000

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-param-reassign*/
+/* eslint-disable no-prototype-builtin*/
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr.filter((obj) => obj.city === location).map((students) => {
+    newGrade.map((studentGrade) {
+      if (studentGrade.studentId === studentId) {
+        
+      }
+    })
+  }
+}
