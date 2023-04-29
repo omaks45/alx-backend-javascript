72b8a74896b30f415bac3851c87cd5092636aac2
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-04-29 22:16:35 +0100

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,18 +1,16 @@
 export default function groceriesList() {
   const lastMap = new Map();
   const obj = {
-    Apple: 10,
+    Apples: 10,
     Tomatoes: 10,
     Pasta: 1,
     Rice: 1,
     Banana: 5,
   };
-
-  /* get the keys in the object into an array */
+  //   get all the keys in the object into an array
   const arr = Array.from(Object.keys(obj));
 
-  /* map through the array and for each of the ele set the value of the key in the lastmap */
-
+  //   map through the array and for each item, set the value to the key in the finalMap
   arr.map((ele) => lastMap.set(ele, obj[ele]));
   return lastMap;
 }
