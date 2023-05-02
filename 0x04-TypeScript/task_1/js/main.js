020c9d0de62ad496c930b1bc389bcada8a091b5c
omaks45 <danielgozie5@gmail.com>
2023-05-02 03:36:29 +0000

    main.js

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -18,3 +18,29 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
+const printTeacher: printTeacherFunction(firstName: string, lastName: string): string => `${firstName.CharAt[0]} ${lastName}`;
+
+// Writing a class
+
+interface classes {
+  firstName: string;
+  lastName: string;
+  workOnHomeWork(): string;
+  displayName(): string;
+}
+// create the class
+class studentClass implements classes {
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+// the methods of the class
+  workOnHomeWork() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
